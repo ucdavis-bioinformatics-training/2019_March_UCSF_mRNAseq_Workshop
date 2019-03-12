6d2a130ab14d70225d90c79452e573473244cb14
Matt Settles <mattsettles@gmail.com>
2019-03-12 09:29:14 -0700

    update scripts to create References folder if it doesn't exist

--- scripts/star_index.sh
+++ scripts/star_index.sh
@@ -6,6 +6,7 @@ start=`date +%s`
 echo $HOSTNAME
 
 outpath="References"
+[[ -d ${outpath} ]] || mkdir ${outpath}
 
 cd ${outpath}
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_29/GRCh38.primary_assembly.genome.fa.gz
